866e9ac37be55f7d29f522af914e1c81130c1471
Tushar Bhosale <114877970+Tusharbhosalehub@users.noreply.github.com>
2024-01-09 23:09:30 +0530

    Update Fork_and_Wait.c

--- Fork_and_Wait.c
+++ Fork_and_Wait.c
@@ -16,6 +16,40 @@ void bubbleSort(int arr[],int n){
  }
  printf("\n");
 }
+void selectionSort(int arr[], int n){
+int min_idx;
+ for (int i = 0; i < n-1; i++){
+ min_idx = i;
+ for (int j = i+1; j < n; j++)
+ if (arr[j] < arr[min_idx])
+ min_idx = j;
+ if(min_idx!=i){
+ int temp = arr[min_idx];
+ arr[min_idx] = arr[i];
+ arr[i] = temp;
+ }
+ }
+ printf("\nSorted array by Selection sort in parent process = ");
+ for(int i = 0;i<n;i++){
+ printf("%d ",arr[i]);
+ }
+ printf("\n");
+}
+int main(){
+ pid_t pid;
+ int n;
+ printf("Enter the number of Elements = ");
+ scanf("%d",&n);
+ int arr[n];
+ printf("Enter the array = ");
+ for(int i = 0;i<n;i++) scanf("%d",&arr[i]);
+ pid = fork();
+ if(pid==0){
+ printf("\n****** CHILD PROCESS ********\n");
+ printf("\nIt is child process with pid = %d and ppid = %d \n",getpid(),getppid());
+ bubbleSort(arr,n);
+ printf("\n ***** CHILD PROCESS TERMINATED *****");
+ }
  else{
  wait(NULL);
  printf("\n\n\n****** Parent PROCESS ********\n");
@@ -24,4 +58,3 @@ void bubbleSort(int arr[],int n){
  printf("\n ***** PARENT PROCESS TERMINATED *****");
  }
 }
-
