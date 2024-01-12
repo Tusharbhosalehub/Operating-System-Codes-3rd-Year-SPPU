52117fca931041750662d083d403fb4961764e5d
Tushar Bhosale <114877970+Tusharbhosalehub@users.noreply.github.com>
2024-01-13 00:54:06 +0530

    Update Producer_&_Consumer.c

--- Producer_&_Consumer.c
+++ Producer_&_Consumer.c
@@ -43,6 +43,31 @@ void* consumer(void* arg) {
     }
     pthread_exit(NULL);
 }
+int main() {
+    int num_producers, num_consumers, num_iterations;
+
+    printf("Enter the number of producers: ");
+    scanf("%d", &num_producers);
+
+    printf("Enter the number of consumers: ");
+    scanf("%d", &num_consumers);
+
+    printf("Enter the number of iterations: ");
+    scanf("%d", &num_iterations);
+
+    pthread_t producers[num_producers];
+    pthread_t consumers[num_consumers];
+
+    // Initialize semaphores and mutex
+    sem_init(&empty, 0, BUFFER_SIZE);
+    sem_init(&full, 0, 0);
+    pthread_mutex_init(&mutex, NULL);
+
+    // Create producer threads
+    for (int i = 0; i < num_producers; i++) {
+        pthread_create(&producers[i], NULL, producer, &num_iterations);
+    }
+
 
 
 
