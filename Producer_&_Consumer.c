dcf21e9c90055ead6639677730710c6f27321ac8
Tushar Bhosale <114877970+Tusharbhosalehub@users.noreply.github.com>
2024-01-13 00:53:17 +0530

    Update Producer_&_Consumer.c

--- Producer_&_Consumer.c
+++ Producer_&_Consumer.c
@@ -10,3 +10,21 @@ int in = 0, out = 0;
 
 sem_t empty, full;
 pthread_mutex_t mutex;
+void* producer(void* arg) {
+    int item;
+    for (int i = 0; i < *((int*)arg); i++) {
+        item = rand() % 100; // Generate a random item
+        sem_wait(&empty);
+        pthread_mutex_lock(&mutex);
+
+        // Produce item
+        printf("Producer produces item: %d\n", item);
+        buffer[in] = item;
+        in = (in + 1) % BUFFER_SIZE;
+
+        pthread_mutex_unlock(&mutex);
+        sem_post(&full);
+    }
+    pthread_exit(NULL);
+}
+
