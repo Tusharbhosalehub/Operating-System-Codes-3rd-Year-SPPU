0c6ed0ca5d12e701d28ee3abb2390f1cc3b2cacd
Tushar Bhosale <114877970+Tusharbhosalehub@users.noreply.github.com>
2024-01-13 00:52:45 +0530

    Update Producer_&_Consumer.c

--- Producer_&_Consumer.c
+++ Producer_&_Consumer.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
 
+#define BUFFER_SIZE 5
+
+int buffer[BUFFER_SIZE];
+int in = 0, out = 0;
+
+sem_t empty, full;
+pthread_mutex_t mutex;
